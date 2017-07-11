3c78ded9c3019f51a4c8d6e9f7f905115507fa75
hello2all <hellot2@hotmail.com>
2017-07-11 18:11:56 +0800

    Add waypoints from json

--- example.py
+++ example.py
@@ -8,6 +8,7 @@ import numpy as np
 from time import sleep
 from path_planner import PathPlanner
 from controller import PurePursuit, LogUtil
+from get_path import get_waypoint_list
 
 # setup logging
 LogUtil.set_up_logging('PurePursuit.txt')
@@ -16,17 +17,17 @@ LogUtil.set_up_logging('PurePursuit.txt')
 world = World()
 # timestep for world update
 dt = 0.1
-goal_tolerance = 0.25
+goal_tolerance = 2
 
 # initialize planner and controller
 # waypoints, goal = PathPlanner.plan(world, 10)
-waypoint_list = [[1.5, 3], [2, 6], [3, 7], [4, 6], [5, 6.5], [6, 8], [8, 8], [9, 6], [6, 4], [7, 3], [8, 2.5]]
+waypoint_list = get_waypoint_list()
 waypoints, goal = PathPlanner.create_waypoints(waypoint_list)
-world.robot = Robot(Pose(waypoints[0].position, np.pi))
+world.robot = Robot(Pose(waypoints[0].position, 0))
 
-max_linear_velocity = 0.5
-max_angular_velocity = np.pi / 3.0
-look_ahead_dist = 0.1
+max_linear_velocity = 1
+max_angular_velocity = np.pi / 6.0
+look_ahead_dist = .01
 controller = PurePursuit(waypoints, max_linear_velocity, max_angular_velocity, look_ahead_dist)
 
 # init pygame screen for visualization
@@ -34,9 +35,9 @@ screen = world.init_screen()
 
 while True:
     # collision testing
-    if world.in_collision():
-        print('Collision')
-        break
+    # if world.in_collision():
+    #     print('Collision')
+    #     break
 
     # check if we have reached our goal
     vehicle_pose = world.robot.pose
